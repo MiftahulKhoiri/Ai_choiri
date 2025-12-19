0389f9db476e39543db189f479bb624ce685c9b5
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-19 19:21:27 +0800

    Create dataset.py

---
+++ ml/dataset.py
@@ -0,0 +1,18 @@
+dataset = {
+    "greeting": [
+        "halo",
+        "hai",
+        "selamat pagi",
+        "apa kabar"
+    ],
+    "goodbye": [
+        "bye",
+        "dadah",
+        "sampai jumpa"
+    ],
+    "python": [
+        "aku suka python",
+        "belajar python",
+        "python itu apa"
+    ]
+}
