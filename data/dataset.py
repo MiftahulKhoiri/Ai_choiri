e702d6a9ad1b8e7fdb466ad9af572c3a6714a945
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-20 01:52:18 +0800

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,29 +1,30 @@
 DATASET = {
     "greeting": [
-        "halo", "hai", "selamat pagi", "selamat malam"
+        "halo",
+        "hai",
+        "selamat pagi",
+        "selamat malam"
     ],
+
     "bye": [
-        "bye", "dadah", "sampai jumpa"
+        "bye",
+        "dadah",
+        "sampai jumpa"
     ],
+
     "python": [
         "apa itu python",
         "jelaskan python",
         "python itu apa"
     ],
+
     "creator_python": [
         "siapa yang bikin python",
         "siapa pembuat python"
     ],
+
     "tanya_waktu": [
         "jam berapa",
         "besok jam berapa"
     ],
-    "greeting": ["halo", "hai"],
-    "bye": ["bye", "dadah"],
-    "perkenalan": [
-        "namaku bagus",
-        "nama saya bagus",
-        "aku bernama bagus"
-    ],
-    "python": ["apa itu python"],
 }
