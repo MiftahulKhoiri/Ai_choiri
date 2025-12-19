4628ce6ff71e8a010a48c8acc11fd74b249f0b11
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-19 14:19:41 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 from utils.updater import git_update
 from core.logic import SimpleAI
 from config.settings import AI_NAME, VERSION
+from data.memory import load_memory, save_memory
 
 def main():
-    # update repo dulu
     git_update()
 
-    # jalankan AI
-    ai = SimpleAI()
+    memory = load_memory()
+    ai = SimpleAI(memory)
 
     print("=" * 30)
     print(f"{AI_NAME} v{VERSION}")
@@ -15,12 +15,15 @@ def main():
     print("=" * 30)
 
     while True:
-        user_input = input("Kamu: ").strip()
-        if not user_input:
+        user = input("Kamu: ").strip()
+        if not user:
             continue
 
-        respon = ai.respond(user_input)
+        respon = ai.respond(user)
         print("AI :", respon)
+
+        memory.append({"user": user, "ai": respon})
+        save_memory(memory)
 
         if respon.lower().startswith("sampai"):
             break
