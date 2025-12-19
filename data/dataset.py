37936dc9c2c6e5a0ba485ba4bff407faaa3ea089
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-20 01:41:28 +0800

    Create dataset.py

---
+++ data/dataset.py
@@ -0,0 +1,21 @@
+DATASET = {
+    "greeting": [
+        "halo", "hai", "selamat pagi", "selamat malam"
+    ],
+    "bye": [
+        "bye", "dadah", "sampai jumpa"
+    ],
+    "python": [
+        "apa itu python",
+        "jelaskan python",
+        "python itu apa"
+    ],
+    "creator_python": [
+        "siapa yang bikin python",
+        "siapa pembuat python"
+    ],
+    "tanya_waktu": [
+        "jam berapa",
+        "besok jam berapa"
+    ]
+}
