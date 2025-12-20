dbf0893f301c429e084581e378440ac7a7fb166d
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-20 12:23:39 +0800

    Update context.py

--- core/context.py
+++ core/context.py
@@ -1,11 +1,14 @@
 from collections import deque
 
 class ContextMemory:
-    def __init__(self, max_len=3):
-        self.memory = deque(maxlen=max_len)
+    def __init__(self):
+        self.memory = []
 
     def add(self, text):
         self.memory.append(text)
 
     def get(self):
         return " ".join(self.memory)
+
+    def clear(self):
+        self.memory = []
